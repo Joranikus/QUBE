a8d0472e3013f2bf53bc0d97b29bec06c9eecfd7
Joranikus <orjan.t.lynnes@ntnu.no>
2024-10-28 11:54:56 +0100

    pid

--- control.py
+++ control.py
@@ -16,9 +16,42 @@ pid = PID()
 
 # Main function to control. Must return the voltage (control signal) to apply to the motor.
 def control_system(dt, motor_angle, pendulum_angle, rpm):
+#0.01 0.03 0.0005
+    global timer
+    global MOTOR_TARGET_ANGLE
+    global I
+    global prev_error
+    global D_filtered
+    global D
 
-    ### WRITE YOUR CODE HERE
-    u = 0
+
+
+    timer += dt
+    if timer > 3:
+        if MOTOR_TARGET_ANGLE < 90:
+            MOTOR_TARGET_ANGLE = 90
+        else:
+            MOTOR_TARGET_ANGLE = 0
+        timer = 0
+
+    error = MOTOR_TARGET_ANGLE - motor_angle
+
+
+    P = pid.kp * error
+    I += pid.ki * error * dt
+    if(I > pid.windup):
+        I = pid.windup
+
+    if I < -pid.windup:
+        I = -pid.windup
+
+    D = pid.kd * (error - prev_error) / dt
+    D_filtered = D *0.5 + D_filtered * 0.5
+
+
+    prev_error = error
+
+    u = P + I + D_filtered
 
     return u
 
